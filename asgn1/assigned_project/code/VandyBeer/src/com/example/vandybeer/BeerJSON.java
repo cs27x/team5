fd92caedcc5ae9aee86673c9b9ece31d60073cb0
Michael Esparza <michael.l.esparza@gmail.com>
2014-09-21 11:18:17 -0500

    changed branch name

--- asgn1/assigned_project/code/VandyBeer/src/com/example/vandybeer/BeerJSON.java
+++ asgn1/assigned_project/code/VandyBeer/src/com/example/vandybeer/BeerJSON.java
@@ -26,16 +26,19 @@ public class BeerJSON {
 	//private final JavaType eventListType = objectMapper.getTypeFactory().constructCollectionType(List.class);
 	//private final BeerJSON value = objectMapper.readValue(new URL(BEER_PERMITS), BeerJSON.class);
 	
+	private static final String BEER_PERMITS = "http://data.nashville.gov/resource/3wb6-xy3j.json";
+	// create object mapper instance
+	private final static ObjectMapper mapper = new ObjectMapper();
+	
 	public static void main(String[] args) throws Exception {
-		String BEER_PERMITS = "http://data.nashville.gov/resource/3wb6-xy3j.json";
+		
 		
 		// Get contents of json as string using commons IO
 		String JsonStr = IOUtils.toString(new URL(BEER_PERMITS));
 		
 		//System.out.println(JsonStr); I have the JsonStr, now what?
 		
-		// create object mapper instance
-		ObjectMapper mapper = new ObjectMapper();
+		
 		
 		// Used to disable feature that causes mapper to break if it encounters an unknown property
 		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
@@ -51,6 +54,13 @@ public class BeerJSON {
 				//nash.add(b);
 			}
 		}
+		BeerJSON be = new BeerJSON();
+		List<BeerLocation> test = be.retrieveData();
+		for(BeerLocation b : test) {
+			//System.out.println(b);
+		}
+		
+		be.addComment("PIZZEREAL", "PBR", "taste like dogwater");
 		
 		// To add comments you do the following:
 		// Will add a function for this later, but this is the gist of it.
@@ -64,14 +74,38 @@ public class BeerJSON {
 				nash.add(b);
 				b.setComment("PBR", "Tastes like you expect. Mildly terrible");
 				b.setComment("bud light", "more like crud light");
-				System.out.println(b.getComment("PBR"));
-				System.out.println(b.getComment("bud light"));
+				//System.out.println(b.getComment("PBR"));
+				//System.out.println(b.getComment("bud light"));
 			}
 		}
 		
 		// TODO figure out the wrapper for JSON if you think we need it.
 		// Right now BeerLocation obj seems useful for now. 
 		
+	}
+	
+	public List<BeerLocation> retrieveData() throws Exception {
+		// Create list from JSON
+		return mapper.readValue(new URL(BEER_PERMITS), mapper.getTypeFactory().constructCollectionType(List.class,
+				BeerLocation.class));
+	}
+	
+	// don't like this function, should use setComment or something. sleep now.
+	public List<BeerLocation> addComment(String bizName, String beerName, String comment) throws Exception {
+		List<BeerLocation> loc = retrieveData();
+		List<BeerLocation> comm = new ArrayList<BeerLocation>();
+		
+		for(BeerLocation b : loc)
+		{
+			if(b.getBusinessName() != null && b.getBusinessName().equals(bizName))
+			{
+				comm.add(b);
+				b.setComment(beerName, comment);
+
+			}
+		}
+		System.out.println(beerName + " @ " + bizName + " has been noted as: " + comment);
+		return comm;
 	}
 	
 
