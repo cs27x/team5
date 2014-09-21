117d2fe9b9ac5344f0309a256ec34f16f851f405
Michael Esparza <michael.l.esparza@gmail.com>
2014-09-20 22:16:45 -0500

    testing json array data manipulation

---
+++ asgn1/assigned_project/code/VandyBeer/src/com/example/vandybeer/BeerJSON.java
@@ -0,0 +1,61 @@
+package com.example.vandybeer;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.Collections;
+import java.util.List;
+
+import android.provider.ContactsContract.CommonDataKinds.Event;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class BeerJSON {
+	//private final JavaType eventListType = objectMapper.getTypeFactory().constructCollectionType(List.class);
+	//private final BeerJSON value = objectMapper.readValue(new URL(BEER_PERMITS), BeerJSON.class);
+	
+	public static void main(String[] args) throws Exception {
+		String BEER_PERMITS = "http://data.nashville.gov/resource/3wb6-xy3j.json";
+		
+		// Get contents of json as string using commons IO
+		String JsonStr = IOUtils.toString(new URL(BEER_PERMITS));
+		
+		// create object mapper instance
+		ObjectMapper mapper = new ObjectMapper();
+		
+		// Used to disable feature that causes mapper to break if it encounters an unknown property
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		
+		// Let's see what type the node is
+		List<BeerLocation> nav = mapper.readValue(JsonStr, mapper.getTypeFactory().constructCollectionType(List.class,
+				BeerLocation.class));
+		for(BeerLocation b : nav)
+		{
+			System.out.println(b);
+		}
+		BeerLocation b = mapper.readValue(JsonStr, BeerLocation.class);
+		
+		
+		    
+		
+		
+		System.out.println("here");
+	}
+	
+
+}
+
+
+
