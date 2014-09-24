0355984c668d61c78788774a14b61601e3c29a9c
Christian Reynolds <christian.b.reynolds@vanderbilt.edu>
2014-09-24 16:52:51 -0500

    Pulling chad's stuff

--- asgn1/assigned_project/code/VandyBeer/src/com/example/vandybeer/MainActivity.java
+++ asgn1/assigned_project/code/VandyBeer/src/com/example/vandybeer/MainActivity.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import tests.BeerLocation;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
